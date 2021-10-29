ced786e3a90bea8b0adb63112ddaf081ae9b6c78
bulakh <bulakh94@gmail.com>
2021-10-29 16:16:03 +0300

    fix: Выносит валидацию Login в отдельный хук

---
+++ project/src/hooks/useLoginValid.js
@@ -0,0 +1,50 @@
+export const blurHandler = (evt, email, setEmail, password, setPassword) => {
+  switch(evt.target.name) {
+    case 'email':
+      setEmail({...email, dirty: true});
+      break
+    case 'password':
+      setPassword({...password, dirty: true});
+      break
+    default:
+  }
+};
+
+export const focusHandler = (evt, email, setEmail, password, setPassword) => {
+  switch(evt.target.name) {
+    case 'email':
+      setEmail({...email, dirty: false});
+      break
+    case 'password':
+      setPassword({...password, dirty: false});
+      break
+    default:
+  }
+};
+
+export const emailHandler = (evt, email, setEmail) => {
+
+  const re = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!re.test(String(evt.target.value).toLowerCase())) {
+    setEmail({...email, error: 'Некорректный емейл'});
+    if (!evt.target.value) {
+      setEmail({...email, error: 'Емейл не может быть пустым'});
+    }
+  } else {
+    setEmail({...email, error: ''});
+    }
+};
+
+export const passwordHandler = (evt, password, setPassword) => {
+  if (evt.target.value.length < 8) {
+    setPassword({...password, error: 'Пароль должен быть длиннее 8 символов'});
+    if (!evt.target.value) {
+      setPassword({...password, error: 'Пароль не может быть пустым'});
+    }
+  } else if (evt.target.value.match(/[а-яА-ЯЁё]/ig) !== null) {
+      setPassword({...password, error: 'Не используйте кириллицу'});
+    } else {
+        setPassword({...password, error: ''});
+      }
+};
--- project/src/pages/Login/Login.jsx
+++ project/src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router';
 import { AppRoute } from '../../const';
 import { useDispatch } from 'react-redux';
 import { changeEmail } from '../../store/actions';
+import { blurHandler, emailHandler, focusHandler, passwordHandler } from '../../hooks/useLoginValid';
 
 function Login() {
   const history = useHistory();
@@ -32,57 +33,6 @@ function Login() {
     }
   }, [emailValid.error, passwordValid.error]);
 
-  const blurHandler = (evt) => {
-    switch(evt.target.name) {
-      case 'email':
-        setEmailValid({...emailValid, dirty: true});
-        break
-      case 'password':
-        setPasswordValid({...passwordValid, dirty: true});
-        break
-      default:
-    }
-  };
-
-  const focusHandler = (evt) => {
-    switch(evt.target.name) {
-      case 'email':
-        setEmailValid({...emailValid, dirty: false});
-        break
-      case 'password':
-        setPasswordValid({...passwordValid, dirty: false});
-        break
-      default:
-    }
-  };
-
-  const emailHandler = (evt) => {
-
-    const re = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    if (!re.test(String(evt.target.value).toLowerCase())) {
-      setEmailValid({...emailValid, error: 'Некорректный емейл'});
-      if (!evt.target.value) {
-        setEmailValid({...emailValid, error: 'Емейл не может быть пустым'});
-      }
-    } else {
-      setEmailValid({...emailValid, error: ''});
-      }
-  };
-
-  const passwordHandler = (evt) => {
-    if (evt.target.value.length < 8) {
-      setPasswordValid({...passwordValid, error: 'Пароль должен быть длиннее 8 символов'});
-      if (!evt.target.value) {
-        setPasswordValid({...passwordValid, error: 'Пароль не может быть пустым'});
-      }
-    } else if (evt.target.value.match(/[а-яА-ЯЁё]/ig) !== null) {
-        setPasswordValid({...passwordValid, error: 'Не используйте кириллицу'});
-      } else {
-          setPasswordValid({...passwordValid, error: ''});
-        }
-  };
-
   return (
     <div className={cn(generalStyles.page, styles.wrap)}>
       <h1 className={generalStyles.hidden}>Вход в Simple Hotel Check</h1>
@@ -95,9 +45,9 @@ function Login() {
             >
               <p>Логин</p>
               <input
-                onChange={emailHandler}
-                onBlur={blurHandler}
-                onFocus={focusHandler}
+                onChange={(evt) => emailHandler(evt, emailValid, setEmailValid)}
+                onBlur={(evt) => blurHandler(evt, emailValid, setEmailValid, passwordValid, setPasswordValid)}
+                onFocus={(evt) => focusHandler(evt, emailValid, setEmailValid, passwordValid, setPasswordValid)}
                 name='email'
                 type='email'
                 autoComplete='email'
@@ -115,9 +65,9 @@ function Login() {
             >
               <p>Пароль</p>
               <input
-                onChange={passwordHandler}
-                onBlur={blurHandler}
-                onFocus={focusHandler}
+                onChange={(evt) => passwordHandler(evt, passwordValid, setPasswordValid)}
+                onBlur={(evt) => blurHandler(evt, emailValid, setEmailValid, passwordValid, setPasswordValid)}
+                onFocus={(evt) => focusHandler(evt, emailValid, setEmailValid, passwordValid, setPasswordValid)}
                 name='password'
                 type='password'
                 className={generalStyles.input}
